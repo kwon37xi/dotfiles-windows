a67e323a30fd22a8204de4994f7e7afd17807647
KwonNam Son <kwon37xi@gmail.com>
2024-12-19 15:04:21 +0900

    탐색기 최근 목록 덜 보이게

--- windows-cofigurations.ps1
+++ windows-cofigurations.ps1
@@ -1,4 +1,17 @@
-# 작업 표시줄(Taskbat) 멀티모니터에서 주 모니터에만 표시하기
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "MMTaskbarEnabled" -Value 0
+# 탐색기 고급 설정 변경.
+$explorerAdvancedRegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+
+## 작업 표시줄(Taskbar) 멀티모니터에서 주 모니터에만 표시하기
+Set-ItemProperty -Path $explorerAdvancedRegPath -Name "MMTaskbarEnabled" -Value 0
+
+## 시작 메뉴에 추천 파일 끄기
+Set-ItemProperty -Path $explorerAdvancedRegPath -Name "Start_TrackDocs" -Value 0
+
+## 파일 탐색기에 최근 파일 끄기
+Set-ItemProperty -Path $explorerAdvancedRegPath -Name "ShowRecent" -Value 0
+
+## 작업패시줄 점프 목록에 항목 표시 갯수 조정
+Set-ItemProperty -Path $explorerAdvancedRegPath -Name "JumpListItems_Maximum" -Value 10
+
 Stop-Process -Name explorer -Force
 # Start-Process를 안해도 자동 재시작됨
